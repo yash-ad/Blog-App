23293ef004f388d0f3064a49f50252f19ea8b2c0
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-05-02 12:43:02 +0530

    Task completed

--- BlogApp/src/App.jsx
+++ BlogApp/src/App.jsx
@@ -1,13 +1,56 @@
-
+import { useState,useEffect } from "react"
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import { login,logout } from "./store/authSlice";
+import Header from './components/index'
+import Footer from './components/index'
+import {Outlet} from 'react-router-dom'
 
 function App() {
 
+//Lets create a loading state ofr handling the network requests, because it takes time for the newtwork request
+//Until then we should handle the network request.
+const [loading,setLoading] = useState(true);
 
-  return (
-    <>
-    <h1>A Blog App with Appwrite</h1>
-    </>
-  )
+//It dispatches an action and calls to the reducer function.
+const dispatch = useDispatch();
+
+//So whenever the application loads or run , should ask to the user if user is available or not
+//Using 'useEffect' Hook:-
+
+useEffect(()=>{
+//authService and its currentUser from appWrite there is method called 'getCurrentUser()':-
+authService.getCurrentUser()
+//If successfully Data is fetched then store the data into the 'userData' its a variable  in the '.then()'
+.then((userData)=>{
+//Lets check for a condition  , is there  'userData' that means the currentUser is then show the data
+if(userData){
+  dispatch(login({userData}))
+}
+//And if there is currentUser is not available then else update the state.
+else{
+dispatch(logout())
+}
+})
+//Because the loading has been finished then update the state using setLoading() to false.
+.finally(()=>{
+  setLoading(false);
+})
+},[])
+
+ //Conditional rendering using ternary operator:-
+ //If loading is not true that means its false then show this or other wise then show this:-
+ return !loading ?(
+  <div className="min-h-screen flex flex-wrap content-between bg-gray-400">Test
+  <div className="w-full block" >
+<Header/>
+<main>
+  <Outlet/>
+</main>
+<Footer/>
+  </div>
+  </div>
+ ) : null
 }
 
 export default App
--- BlogApp/src/store/authSlice.js
+++ BlogApp/src/store/authSlice.js
@@ -10,12 +10,25 @@ const initialState = {
 
 
 const authSlice = createSlice({
+    //Slice name
     name:'auth',
+    //Slice initialState
     initialState,
+    //Slice reducers or actions
     reducers:{
-
+        //Individual functions to access from components:-
+        //And we know that every reducer has an access of state and action
+login:(state,action)=>{
+state.status = true;
+state.userData = action.payload;
+},
+logout:(state)=>{
+state.status = false;
+state.userData = null;
+}
     }
 })
 
 
-
+export const {login,logout} = authSlice.actions;
+export default authSlice.reducer;
