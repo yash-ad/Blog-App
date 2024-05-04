ddcdf07dd9cb9c873adc9f9b37aa2383d1ae5d70
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-05-04 18:18:39 +0530

    Created a Button component to implement the Button in our project

---
+++ BlogApp/src/components/Button.jsx
@@ -0,0 +1,19 @@
+
+
+//Accept properties(props) as a parameters:-
+function Button({
+    //Also known as 'children' in a programming.
+buttonText,
+type = 'button',
+bgColor = 'bg-blue-600',
+textColor = 'text-white',
+className ='',
+...props
+}) {
+  return (
+    //Adding variables into the className that we have created in a properties in a paramter of a Button.
+    <Button className={`px-4 py-2 rounded-lg ${bgColor} ${className} ${type} ${textColor}`} {...props}>{buttonText}</Button>
+  )
+}
+
+export default Button
