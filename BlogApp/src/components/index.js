0abdd878e87f2a51e2e6736426ae9802347ad5e6
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-05-07 22:19:20 +0530

    code updated with a few changes

--- BlogApp/src/components/index.js
+++ BlogApp/src/components/index.js
@@ -3,6 +3,7 @@ import Footer from "./Footer/Footer";
 import Container from './Container/Container'
 import Logo from    './Logo';
 import LogoutBtn from './Header/LogoutBtn'
+import Postcard from './PostCard'
 
 
 export {
@@ -10,5 +11,6 @@ export {
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Postcard
 };
