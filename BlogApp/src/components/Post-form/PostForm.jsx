d345906d8fc641f7a0bc0aff74e8a8f73b508716
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-05-11 12:38:36 +0530

    I have cleaned up the code and updated it.

--- BlogApp/src/components/Post-form/PostForm.jsx
+++ BlogApp/src/components/Post-form/PostForm.jsx
@@ -1,187 +1,119 @@
-import { useCallback,useEffect } from "react";
-import{useNavigate} from 'react-router-dom';
-import {Button,Input,Select,RealTimeEditor} from '../index'
-import {useSelector} from 'react-redux';
+import { useCallback, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import { Button, Input, Select, RealTimeEditor } from '../index';
+import { useSelector } from 'react-redux';
 import appWriteService from '../../appwrite/conf.js';
 import { useForm } from "react-hook-form";
 
-
-
-
-//Values are from 'post' the user has passed some information.
-function PostForm({post}) {
-
-//Initially lets write some functionalities:-
-
-//1.Information from React-hook-form:-
- //What is 'watch' from React-hook-form?
- //Watch is basically used for subscribe to input changes.
- //What is setValue from React-hook-form?
- //'setValue' is used for update field value.
- //What is 'getValues' from React-hook-form?
- //'getValues' is used for get form values.
- //'useForm' takes an object.
-const {register,handleSubmit,watch,control,setValue,getValues} = useForm({
-    defaultValues:{
-        //If the user's information is coming from post then show the previous post or else keep it empty.
-        title:post?.title || '',
-        slug:post?.slug || '',
-        content:post?.content || '',
-        status:post?.status || 'active'
-    }
-})
-
-const navigate = useNavigate();
-
-const userData = useSelector((state)=> state.auth.userData)
-
-
-//If the user has been submitted the form , obviously passed the 'data'.
-const submit = async(data)=>{
-
-    //If there is a post , then update the file:-
-    if(post){
-  // If there is an image, upload the file, otherwise set file to null
-    const file = data.image[0] ? appWriteService.uploadFile(data.image[0]) : null
-
-    // If there is a previous post available, delete the older image of that post
-    if(file){
-        appWriteService.deleteFile(post.featuredImage)
-    }
-
-    //Update the post , 'updatePost()' from appWrite service, it takes slug that means its an id of post:-
-    // Here the the slug is 'post.$id
-    //Spreading the existing data with an overwriting value featuredImage
-    // if there is a file then there that file has an uniqueId that is stored into the featuredImage and if its not the its undefined
-    const dataBasePost = await appWriteService.updatePost(post.$id, {
-        ...data,
-        featuredImage: file ? file.$id : undefined
-    });
-    // Navigate then user to the updated post
-        if(dataBasePost){
-            navigate(`/post/${dataBasePost.$id}`);
+function PostForm({ post }) {
+    const { register, handleSubmit, watch, control, setValue, getValues } = useForm({
+        defaultValues: {
+            title: post?.title || '',
+            slug: post?.slug || '',
+            content: post?.content || '',
+            status: post?.status || 'active'
         }
+    });
 
-    }
-    //What else if there is post not available:-
-    //User wants to create a new file and then upload it.
-    //So lets create a file 
-    else{
-        
-// const file = data.image[0]? await appWriteService.uploadFile(data.image[0]) : null
-//uploadFile is likely an asynchronous operation, 
-//meaning it returns a Promise.
-// To properly handle asynchronous operations, 
-//you should use await to wait for the Promise to resolve.
-//await should be used when calling an asynchronous function to ensure proper sequencing of operations.
-// If uploadFile encounters an error, using await allows you to catch and handle it using try...catch. This is essential for robust error handling in asynchronous code..
-const file = await appwriteService.uploadFile(data.image[0]);
+    const navigate = useNavigate();
+    const userData = useSelector((state) => state.auth.userData);
 
-//If there is a file then store into the fileId:-
-if(file){
-    const fileId = file.$id;
-    //Data then there is a featuredImage then update the fileId:-
-data.featuredImage = fileId;
-//Updated the property and rest of the properties should be update:-
-//Send the data to appWrite service and we know that app createPost() from appWrite its an asynchronous operation
-//That means it returns a promise 
-// To properly handle asynchronous operations, 
-//We should use await to wait for the promise to resolve
-//await should be used when calling an asychronous operation:-
-const dbPost = await appWriteService.createPost({
-    ...data,
-    userId:userData.$id
-})
+    const submit = async (data) => {
+        if (post) {
+            const file = data.image[0] ? appWriteService.uploadFile(data.image[0]) : null;
+            if (file) {
+                appWriteService.deleteFile(post.featuredImage);
+            }
+            const dataBasePost = await appWriteService.updatePost(post.$id, {
+                ...data,
+                featuredImage: file ? file.$id : undefined
+            });
+            if (dataBasePost) {
+                navigate(`/post/${dataBasePost.$id}`);
+            }
+        } else {
+            const file = await appWriteService.uploadFile(data.image[0]);
+            if (file) {
+                const fileId = file.$id;
+                data.featuredImage = fileId;
+                const dbPost = await appWriteService.createPost({
+                    ...data,
+                    userId: userData.$id
+                });
+                if (dbPost) {
+                    navigate(`/post/${dbPost.$id}`);
+                }
+            }
+        }
+    };
 
-if(dbPost){
-    navigate(`/post/${dbPost.$id}`);
-}
-}
-}
-    
-} 
+    const slugTransform = useCallback((value) => {
+        if (value && typeof value === 'string') {
+            return value.trim().toLowerCase().replace(/[^a-zA-Z\d\s]+/g, "-").replace(/\s/g, "-");
+        }
+        return "";
+    });
 
-///.One:-Interesting Concept to crack the interviews:-
-const slugTransform = useCallback((value)=>{
-//If there ia 'value' available && which is true and the value is the type of is strict equals to the string then return the value:-
-if(value && typeof value === 'string'){
-    return value
-    .trim()
-    .toLowerCase()
-    //This is a regular expression to check:-
-    .replace(/[^a-zA-Z\d\s]+/g, "-")
-    .replace(/\s/g, "-");
-}
-//If its not the returns an empty string:-
-return "";
-})
+    useEffect(() => {
+        const subscription = watch((value, { name }) => {
+            if (name === 'title') {
+                setValue('slug', slugTransform(value.title), { shouldValidate: true });
+            }
+        });
+        return () => {
+            subscription.unsubscribe();
+        };
+    }, [watch, slugTransform, setValue]);
 
-
-///.Two:-
-useEffect(()=>{
-    const subscription = watch((value,{name})=>{
-if(name === 'title'){
-    setValue('slug',slugTransform(value.title),{
-        shouldValidate : true })
-}
-})
-    //For optimization:-
-    return ()=>{
-        subscription.unsubscribe();
-    }
-},[watch,slugTransform,setValue])
-
-
-  return (
-    //The form is divided into two parts:-
-    <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
-    <div className="w-2/3 px-2">
-        <Input
-            label="Title :"
-            placeholder="Title"
-            className="mb-4"
-            {...register("title", { required: true })}
-        />
-        <Input
-            label="Slug :"
-            placeholder="Slug"
-            className="mb-4"
-            {...register("slug", { required: true })}
-            onInput={(e) => {
-                setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true });
-            }}
-        />
-        <RealTimeEditor label="Content :" name="content" control={control} defaultValue={getValues("content")} />
-    </div>
-    <div className="w-1/3 px-2">
-        <Input
-            label="Featured Image :"
-            type="file"
-            className="mb-4"
-            accept="image/png, image/jpg, image/jpeg, image/gif"
-            {...register("image", { required: !post })}
-        />
-        {post && (
-            <div className="w-full mb-4">
-                <img
-                    src={appWriteService.getFilePreview(post.featuredImage)}
-                    alt={post.title}
-                    className="rounded-lg"
+    return (
+        <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+            <div className="w-2/3 px-2">
+                <Input
+                    label="Title :"
+                    placeholder="Title"
+                    className="mb-4"
+                    {...register("title", { required: true })}
                 />
+                <Input
+                    label="Slug :"
+                    placeholder="Slug"
+                    className="mb-4"
+                    {...register("slug", { required: true })}
+                    onInput={(event) => {
+                        setValue("slug", slugTransform(event.currentTarget.value), { shouldValidate: true });
+                    }}
+                />
+                <RealTimeEditor label="Content :" name="content" control={control} defaultValue={getValues("content")} />
             </div>
-        )}
-        <Select
-            options={["active", "inactive"]}
-            label="Status"
-            className="mb-4"
-            {...register("status", { required: true })}
-        />
-        <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full">
-            {post ? "Update" : "Submit"}
-        </Button>
-    </div>
-</form>
-  )
+            <div className="w-1/3 px-2">
+                <Input
+                    label="Featured Image :"
+                    type="file"
+                    className="mb-4"
+                    accept="image/png, image/jpg, image/jpeg, image/gif"
+                    {...register("image", { required: !post })}
+                />
+                {post && (
+                    <div className="w-full mb-4">
+                        <img
+                            src={appWriteService.getFilePreview(post.featuredImage)}
+                            alt={post.title}
+                            className="rounded-lg"
+                        />
+                    </div>
+                )}
+                <Select
+                    options={["active", "inactive"]}
+                    label="Status"
+                    className="mb-4"
+                    {...register("status", { required: true })}
+                />
+                <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full">
+                    {post ? "Update" : "Submit"}
+                </Button>
+            </div>
+        </form>
+    );
 }
 
-export default PostForm
+export default PostForm;
--- BlogApp/src/components/RealTimeEditor.jsx
+++ BlogApp/src/components/RealTimeEditor.jsx
@@ -3,19 +3,24 @@ import {Editor} from '@tinymce/tinymce-react';
 import {Controller} from 'react-hook-form';
 
 
-//Take inputs as a props into the parameter of a function called RealTimeEditor().
+// It takes several props as input: name, control, label, and defaultValue.
 // What is control? 
 //control which comes from react-hook-form it is responsible for controlling the form.
 function RealTimeEditor({name,control,label,
     defaultValue="",}) {
   return (
 <div className='w-full'>
+{/*Within the JSX, it checks if a label prop is provided. If it is, it renders a label element. */}
 {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+{/*Then it uses the Controller component from react-hook-form to wrap the Editor component. */}
+{/* The Controller component manages the interaction between the Editor component and the form's state managed by react-hook-form. */}
 <Controller
 name={name || 'content'}
 control={control}
 render={({field:{onChange}})=>(
+  // The Editor component is the actual rich text editor provided by TinyMCE.
     <Editor
+    //The initialValue prop is set to the provided defaultValue, which is either an empty string or a default value specified by the parent component.
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
@@ -47,13 +52,15 @@ render={({field:{onChange}})=>(
             "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
             content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
         }}
+//The onEditorChange event handler is provided, which will be called whenever the 
+//content of the editor changes. Inside this handler, it invokes the onChange function provided by Controller      
+// ensuring that any changes in the editor are reflected in the form's state managed by react-hook-form.      
         onEditorChange={onChange}
         />
 )}
-
 />
 </div>
   )
 }
 
-export default RealTimeEditor
+export default RealTimeEditor;
