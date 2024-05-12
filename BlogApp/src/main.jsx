a1d927077f7f682b4d484ed6cbd44de0e6c8c395
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-05-12 16:56:26 +0530

    Setting routes for raect router dom

--- BlogApp/src/main.jsx
+++ BlogApp/src/main.jsx
@@ -4,10 +4,22 @@ import App from './App.jsx'
 import './index.css';
 import {Provider} from 'react-redux';
 import {store} from './store/store.js'
+import { Route, RouterProvider,createBrowserRouter,createRoutesFromElements } from 'react-router-dom';
+import Home from "./pages/Home"
 
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<App/>}>
+<Route path='/' element={<Home/>}/> 
+<Route path= "" element={}/> 
+<Route path='' element={}/> 
+<Route path='' element={}/> 
+<Route path='' element={}/> 
+  </Route>
+))
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <Provider store={store}>
+  <RouterProvider router={router}/>
   <App />
   </Provider>
   </React.StrictMode>,
