8f68238d4adeabce93fee2f865934eb972d38fef
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-05-12 16:05:49 +0530

    Home page for Blog app

---
+++ BlogApp/src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import { useState,useEffect } from "react";
+import appWriteService from '../appwrite/conf.js';
+import {Container,PostCard} from '../components'
+
+function Home() {
+
+    const [posts,setPosts] = useState([]);
+
+    useEffect(()=>{
+appWriteService.getPosts().then((posts)=>{
+if(posts){
+    setPosts(posts.documents)
+}
+})
+    },[]);
+
+    return posts.length === 0 ? (
+        <div className="w-full py-8 mt-4 text-center">
+        <Container>
+        <div className="flex flex-wrap">
+                                <div className="p-2 w-full">
+                                    <h1 className="text-2xl font-bold hover:text-gray-500">
+                                        Login to read posts
+                                    </h1>
+                                </div>
+                            </div>
+        </Container>
+                </div>
+        
+
+    )  : (
+        <div className='w-full py-8'>
+        <Container>
+        <div className='flex flex-wrap'>
+{posts.map((post)=>(
+<div key={post.$id} className='p-2 w-1/4'>
+<PostCard {...posts}/>
+</div>
+))}
+        </div>
+        </Container>
+    </div>
+    )
+}
+
+export default Home
