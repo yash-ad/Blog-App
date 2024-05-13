958c40a30eb4c556ef8e6e7132940f2b9d5f6758
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-05-13 20:46:19 +0530

    Debugging

--- BlogApp/src/main.jsx
+++ BlogApp/src/main.jsx
@@ -43,8 +43,7 @@ const router = createBrowserRouter([
         {
             path: "/all-posts",
             element: (
-                <AuthLayout authentication>
-                    {" "}
+                <AuthLayout authentication={true}>
                     <AllPosts />
                 </AuthLayout>
             ),
@@ -52,8 +51,8 @@ const router = createBrowserRouter([
         {
             path: "/add-post",
             element: (
-                <AuthLayout authentication>
-                    {" "}
+                <AuthLayout authentication={true}>
+               
                     <AddPost />
                 </AuthLayout>
             ),
@@ -61,8 +60,8 @@ const router = createBrowserRouter([
         {
             path: "/edit-post/:slug",
             element: (
-                <AuthLayout authentication>
-                    {" "}
+                <AuthLayout authentication={true}>
+                  
                     <EditPost />
                 </AuthLayout>
             ),
