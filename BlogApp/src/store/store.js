2d67371433c3ced1f69e56b1f59bd345fabbf679
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-05-20 14:38:12 +0530

    craeted postSlice and in the store added postReducer from combineReducers

--- BlogApp/src/store/store.js
+++ BlogApp/src/store/store.js
@@ -1,15 +1,23 @@
 // Import necessary function from Redux Toolkit for creating the store
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore,combineReducers } from '@reduxjs/toolkit';
 
-// Import the authReducer from authSlice
+// Import the authReducer from authSlice:-
 import authReducer from './authSlice';
 
-// Configure the Redux store
-const store = configureStore({
-    reducer: {
-        auth: authReducer, // Set the authReducer under the 'auth' slice of the store
-    }
-});
+//Import the postReducer from postSlice:-
+import postReducer from './postSlice'
+
+//combineReducers for more than one reducer:-
+const rootReducer = combineReducers({
+    auth: authReducer,
+    posts: postReducer,
+  });
+
+  //Configure the Redux store
+  const store = configureStore({
+    reducer: rootReducer,
+  });
+
 
 // Export the configured Redux store
 export default store;
